5ab053dc1167828001ed944be94375033c9f8045
gustavo <guhhpj@gmail.com>
2025-03-25 20:24:45 -0300

    chore: first commit

---
+++ src/main/java/com/dio/medical_appointments/DecolaTech2025MedicalAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.dio.medical_appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTech2025MedicalAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTech2025MedicalAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/medical_appointments/DecolaTech2025MedicalAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.medical_appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTech2025MedicalAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
