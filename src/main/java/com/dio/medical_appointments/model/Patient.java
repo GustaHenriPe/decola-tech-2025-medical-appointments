b3f9ff195d34c2ff238fc858956dcb675681f3eb
gustavo <guhhpj@gmail.com>
2025-03-26 19:05:30 -0300

    feat(patient): add patient entity and repository

---
+++ src/main/java/com/dio/medical_appointments/model/Patient.java
@@ -0,0 +1,27 @@
+package com.dio.medical_appointments.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "patient")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String cellphone;
+    private String email;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+
+
+}
