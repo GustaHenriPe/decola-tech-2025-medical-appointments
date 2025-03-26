e550db770266d6908e7f6ae5db172d7da01854ee
gustavo <guhhpj@gmail.com>
2025-03-26 19:08:39 -0300

    feat(Doctor): add Consultation entity and repository and Doctor/Patient relationships.

---
+++ src/main/java/com/dio/medical_appointments/model/Consultation.java
@@ -0,0 +1,29 @@
+package com.dio.medical_appointments.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "consultation")
+public class Consultation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+    @Column(name = "scheduled_date_time")
+    private LocalDateTime scheduledDateTime;
+    private String reason;
+    private String status;
+}
