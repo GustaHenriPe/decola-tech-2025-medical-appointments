2b74a4efee5351ddb0025b39419ebac9a1798863
gustavo <guhhpj@gmail.com>
2025-03-27 21:47:43 -0300

    feat: add GlobalExceptionHandler for centralized exception handling, and creates a custom Exception.

---
+++ src/main/java/com/dio/medical_appointments/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.dio.medical_appointments.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
